8382419cdcfd1e4f801dd6afa930fd4f1c1877d4
esroleo <esroleo@gmail.com>
2021-02-22 23:27:40 -0500

    foreign key added to candidates table and refined queries

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,16 @@
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS candidates;
+
 CREATE TABLE IF NOT EXISTS candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  industry_connected BOOLEAN NOT NULL,
+  party_id INTEGER UNSIGNED,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
+
 
 CREATE TABLE IF NOT EXISTS parties (
   id INTEGER PRIMARY KEY,
--- server.js
+++ server.js
@@ -17,14 +17,17 @@ const db = new sqlite3.Database('./db/election.db', err => {
     if (err) {
       return console.error(err.message);
     }
-  
     console.log('Connected to the election database.');
   });
 
 // Get all candidates
 // req will not be used as we are do not need the argument from the params.
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
       if (err) {
@@ -41,8 +44,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
     const params = [req.params.id]; // *** req.parameters contain the id sent from the fetch or call.
     db.get(sql, params, (err, row) => {
       if (err) {
